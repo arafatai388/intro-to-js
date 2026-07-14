784eb814aa7103208f00d8e821b4956664731196
arafatai388 <arafat.cseai@gmail.com>
2026-07-14 21:45:24 +0600

    Complete JavaScript variable naming rules practice

---
+++ naming.js
@@ -0,0 +1,53 @@
+let location = 'BBan';
+let age = 18;
+let boyos = 18;
+
+// 1. variable name should be something meaningful. it will indicate what it contains
+let togodutogudutogutugudodu = 97;
+let avc = 45;
+let info = 'mon majar';
+let brb = 'Jasmin';
+
+// do not use shorthand.
+// its oky if the name is longer
+
+// 2. can not use a keyword ( reserved word)
+//const let = 26; wrong
+//console.log(let);  wrong
+
+// const false = 'False';  wrong
+
+// 3. has to be in one word. can not contain any spage or gap
+//const her phone number = 5144442111;  wrong
+const her_phone_number = 5144442111; //snake case
+const herPhoneNumber = 5144442111; // camel case. usually this one we will use.
+const HerPhoneNumber = 5144442111; // pascal case
+
+// 4. name cannot start with a number. but number could be used anywhere other than the first letter
+
+//const 3Person = 3;  wrong
+const person3 = 6;
+const p3erson = 6;
+
+// 5. other than letter name can start with either _ , $. cannot use any other special character. *&#@
+
+// 6. case sensive
+
+let address = 'katabon';
+let Address = 'bandroban';
+let AddRess = 'shalbon';
+
+/*
+let
+const
+if
+else
+for
+while
+function
+return
+true
+false
+class
+new
+*/
