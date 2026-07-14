e0e2ac634278586447ff948088864902e9cdb64f
arafatai388 <arafat.cseai@gmail.com>
2026-07-14 20:57:45 +0600

    variable

---
+++ variable.js
@@ -0,0 +1,18 @@
+//VARIABLE
+//To declare a varible in js we need 5 elementsva
+var weight = 38;
+var temperature = 38;
+var people = 38;
+
+console.log(people)
+
+
+// let, const
+//using let it will be changed
+let price = 100;
+price = 50;
+price = 200;
+
+//using const it will be fixed 
+
+const PI = 3.14;
