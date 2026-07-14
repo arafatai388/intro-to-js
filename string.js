dd8e9e2c24f34d3f3fa03b52b9a5e51d749903e1
arafatai388 <arafat.cseai@gmail.com>
2026-07-14 21:14:50 +0600

    string output

---
+++ string.js
@@ -0,0 +1,9 @@
+//string output
+let name = "kalam";
+console.log(name);
+
+let country = "Bangladesh";
+console.log(country);
+//also use single cotation
+let learning = 'JavaScript Programming  language. another thing.';
+console.log(learning);
