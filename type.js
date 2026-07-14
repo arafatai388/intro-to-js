516ecd1c8ef5ead4c89bd51a44db72ece778628a
arafatai388 <arafat.cseai@gmail.com>
2026-07-14 21:24:57 +0600

    data type of Js

---
+++ type.js
@@ -0,0 +1,30 @@
+
+
+const mangoes = 3;
+//Data type
+console.log(typeof mangoes);
+const fruit = 'vangi';
+console.log(typeof fruit);
+
+
+//Data type of JS
+const age = 25;
+console.log(typeof age); // number
+
+const name = "Arafat";
+console.log(typeof name); // string
+
+const isStudent = true;
+console.log(typeof isStudent); // boolean
+
+let city;
+console.log(typeof city); // undefined
+
+const value = null;
+console.log(typeof value); // object
+
+const bigNumber = 12345678901234567890n;
+console.log(typeof bigNumber); // bigint
+
+const id = Symbol("id");
+console.log(typeof id); // symbol
